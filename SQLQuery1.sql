255fbc3e3ce570842b0834a1fa5746463e8cf123
rasipriyanka <priyankapendela111@gmail.com>
2022-02-17 19:45:53 +0530

    Implemented the mysql Retrieve the values

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,5 @@
 create database payroll_service;
 
-
 create table employee_payrol(
  Id int Identity(1,1)PRIMARY KEY,
  Name varchar(250),
@@ -8,5 +7,5 @@ create table employee_payrol(
  StartDate date,
  )
  use payroll_service;
-  select * from employee_payrol;
  Insert into employee_payrol(Name,Salary,StartDate) values('rasi',30000,'02-10-2020');
+ select * from employee_payrol;
