39ba12823fc633ffa4b39271a9c14ecf8068d9aa
rasipriyanka <priyankapendela111@gmail.com>
2022-02-17 20:18:23 +0530

    Implemented the mysql uc5

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,4 +8,7 @@ create table employee_payrol(
  )
  use payroll_service;
  Insert into employee_payrol(Name,Salary,StartDate) values('rasi',30000,'02-10-2020');
+ Insert into employee_payrol(Name,Salary,StartDate) values('priya',15000,'02-12-2020'),('priyanka',33000,'01-03-2021'),('mahesh',44000,'03-02-2021'),('yesh',6600,'05-04-2021');
  select * from employee_payrol;
+ select Salary from employee_payrol where Name='priya' or Name='yesh';
+ select * from employee_payrol where StartDate between cast('2021-01-03' as date) and getdate();
