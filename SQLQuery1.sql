da8310cd52221ba219f5a138c66e14decbe9730c
rasipriyanka <priyankapendela111@gmail.com>
2022-02-19 16:10:12 +0530

    Implementation For Use Case 11

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,5 +1,4 @@
 create database payroll_service;
-
 create table employee_payrol(
  Id int Identity(1,1)PRIMARY KEY,
  Name varchar(250),
@@ -33,6 +32,52 @@ create table employee_payrol(
  select * from employee_payrol;
  Alter table employee_payrol add Deduction bigint,Taxable bigint not null default(45000),IncomeTax bigint not null default(20000);
  update employee_payrol set Deduction=5000;
-Alter table employee_payrol add Netpay bigint;
-update employee_payrol set Netpay=Basepay-Deduction;
+ Alter table employee_payrol add Netpay bigint;
+ update employee_payrol set Netpay=Basepay-Deduction;
 
+-------UC11----------
+create table Companyname(
+CompanyId int not null PRIMARY KEY,
+ComapenyName varchar(200)
+);
+create table EmployeeInfo(
+EmpId int not null PRIMARY KEY,
+CompanyId int,
+EmpName varchar(200),
+EmpPhno bigint,
+StartDate date
+);
+ALTER TABLE EmployeeInfo ADD CONSTRAINT FK_CompanyEmpInfo FOREIGN KEY (CompanyId) REFERENCES Companyname(CompanyId);
+
+create table DepartmentInfo(
+DepartId int not null PRIMARY KEY,
+EmpId int,
+DepertName varchar(200),
+);
+Alter table DepartmentInfo add constraint FK_EmpDeprtInfo foreign key(EmpId)references EmployeeInfo(EmpId);
+
+create table payroll_Emp(
+RFERId int not null PRIMARY KEY,
+EmpId int,
+Basicpay bigint not null,
+Deduction bigint not null,
+Tax  bigint not null,
+Incometax bigint not null,
+Netpay bigint not null
+);
+ALTER TABLE payroll_Emp ADD CONSTRAINT FK_PayrollEMp FOREIGN KEY(EmpId) REFERENCES EmployeeInfo(EmpId);
+ 
+ Insert INTO Companyname(CompanyId,ComapenyName)
+ values(71,'BL');
+
+INSERT INTO EmployeeInfo(EmpId,CompanyId,EmpName,Empphno,StartDate)values(2013346,71,'priyaa',8887779966,'10-6-2020'),(108978,71,'vedha',9234566789,'11-4-2019'),(201837,71,'yesh',9876556890,'5-6-2018');
+
+select * from EmployeeInfo
+
+INSERT INTO  DepartmentInfo(DepartId,EmpId,DepertName)
+values(8,2013346,'admin');
+SELECT * FROM DepartmentInfo
+
+INSERT INTO payroll_Emp(RFERId,EmpId,Basicpay,Deduction,Tax,Incometax,Netpay)values(13445,2013346,76544,2000,2000,1500,45000),(56789,2013346,50000,2000,2000,1500,450000);
+
+SELECT * FROM payroll_Emp
